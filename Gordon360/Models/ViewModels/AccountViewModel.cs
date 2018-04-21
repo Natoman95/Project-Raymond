20dabca963c435c79e3e2b7a18fbdb37de985aef
Nathan Gray <natoman95@gmail.com>
2018-04-21 16:51:19 -0400

    adding back view models and other necessary files

---
+++ Gordon360/Models/ViewModels/AccountViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gordon360.Models.ViewModels
+{
+    public class AccountViewModel
+    {
+        public string GordonID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ADUserName { get; set; }
+        public string AccountType { get; set; }
+        public string Barcode { get; set; }
+        public int show_pic { get; set; }
+        public int ReadOnly { get; set; }
+
+        public static implicit operator AccountViewModel(ACCOUNT a)
+        {
+            AccountViewModel vm = new AccountViewModel
+            {
+                GordonID = a.gordon_id.Trim(),
+                FirstName = a.firstname.Trim(),
+                LastName = a.lastname.Trim(),
+                Email = a.email ?? "", // Some random records have null for an email.
+                ADUserName = a.AD_Username.Trim() ?? "",
+                AccountType = a.account_type.Trim(),
+                Barcode = a.barcode ?? "",
+                show_pic = a.show_pic,
+                ReadOnly = a.ReadOnly
+            };
+
+            return vm;
+        }
+    }
+
+    
+}
---
+++ Gordon360/Models/ViewModels/ActivityViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gordon360.Models.ViewModels
+{
+    public class ActivityViewModel
+    {
+        public string ACT_CDE { get; set; }
+        public string ACT_DESC { get; set; }
+
+        public static implicit operator ActivityViewModel(ACT_INFO a)
+        {
+            ActivityViewModel vm = new ActivityViewModel
+            {
+                ACT_CDE = a.ACT_CDE.Trim(),
+                ACT_DESC = a.ACT_DESC.Trim()
+            };
+
+            return vm;
+        }
+    }
+}
